352d4b784bf3ecc73addaee4738c31ca721da52c
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-17 22:48:33 +0000

    :lipstick: add modal from example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,3 @@
 <main>
 	<slot />
 </main>
-
-<style>
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,10 +22,12 @@
 			text: 'FEED'
 		}
 	];
+
+	let modalEl: HTMLDialogElement | null;
 </script>
 
 <div class="container">
-	<button class="button">Add status</button>
+	<button class="button" on:click={() => modalEl?.showModal()}>Add status</button>
 	<div class="u-flex u-flex-vertical u-gap-16 u-margin-block-start-32">
 		{#each mockStatus as status}
 			<div class="box u-flex u-cross-center u-gap-24">
@@ -40,6 +42,28 @@
 		{/each}
 	</div>
 </div>
+
+<dialog class="modal" bind:this={modalEl}>
+	<form class="modal-form" method="dialog">
+		<header class="modal-header">
+			<div class="avatar is-color-orange is-medium">
+				<span class="icon-exclamation" aria-hidden="true" />
+			</div>
+			<h4 class="modal-title heading-level-5">Modal title</h4>
+			<button class="x-button" aria-label="Close modal">
+				<span class="icon-x" aria-hidden="true" />
+			</button>
+		</header>
+		<div class="modal-content u-small">Modal label text.</div>
+		<div class="modal-footer">
+			<div class="u-flex u-main-end u-gap-16">
+				<button class="button is-text"><span class="text">Button</span></button><button
+					class="button is-secondary"><span class="text">Button</span></button
+				>
+			</div>
+		</div>
+	</form>
+</dialog>
 
 <style>
 	.icon-wrapper {
