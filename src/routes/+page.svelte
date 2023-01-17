29d11540699a6c03ebb45ab5be9ef4205e35dd96
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-17 22:31:55 +0000

    :lipstick: add base styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,14 +25,32 @@
 </script>
 
 <div class="container">
-	<button class="">Add status</button>
-	{#each mockStatus as status}
-		<div class="">
-			<span class="">
-				<span class={`${status.icon}`} />
-			</span>
-			<span>{status.name}</span>
-			<span>{status.text}</span>
-		</div>
-	{/each}
+	<button class="button is-secondary">Add status</button>
+	<div class="u-flex u-flex-vertical u-gap-16 u-margin-block-start-32">
+		{#each mockStatus as status}
+			<div class="box u-flex u-cross-center u-gap-12">
+				<div class="icon-wrapper">
+					<span class={`icon-${status.icon}`} />
+				</div>
+				<div class="u-flex u-flex-vertical">
+					<h6 class="heading-level-6">{status.name}</h6>
+					<p>{status.text}</p>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
+
+<style>
+	.icon-wrapper {
+		display: grid;
+		place-items: center;
+
+		--size: 3rem;
+		width: var(--size);
+		height: var(--size);
+
+		background-color: hsl(var(--color-neutral-30));
+		border-radius: var(--border-radius-circular);
+	}
+</style>
