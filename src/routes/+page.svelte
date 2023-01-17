76f9493673b1ce8df336c297b5fba7960101cad1
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-17 23:43:27 +0000

    :sparkles: functionality and animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import IconPicker from '$components/IconPicker.svelte';
+	import { fly, slide } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 
 	type Status = {
 		icon: string;
@@ -7,7 +9,17 @@
 		text: string;
 	};
 
-	const mockStatus: Status[] = [
+	const isStatus = (status: unknown): status is Status => {
+		return (
+			typeof status === 'object' &&
+			status !== null &&
+			'icon' in status &&
+			'name' in status &&
+			'text' in status
+		);
+	};
+
+	let mockStatus: Status[] = [
 		{
 			name: 'Thomas',
 			icon: 'chat',
@@ -26,13 +38,32 @@
 	];
 
 	let modalEl: HTMLDialogElement | null;
+
+	const handleSubmit = (e: Event) => {
+		e.preventDefault();
+		const form = e.target as HTMLFormElement;
+		const formData = new FormData(form);
+		const data = Object.fromEntries(formData.entries());
+
+		if (!isStatus(data)) {
+			console.error('Invalid status:', data);
+			return;
+		}
+
+		mockStatus = [data, ...mockStatus];
+		modalEl?.close();
+	};
 </script>
 
 <div class="container">
 	<button class="button" on:click={() => modalEl?.showModal()}>Add status</button>
 	<div class="u-flex u-flex-vertical u-gap-16 u-margin-block-start-32">
-		{#each mockStatus as status}
-			<div class="box u-flex u-cross-center u-gap-24">
+		{#each mockStatus as status, i (`${status.name}-${status.text}`)}
+			<div
+				class="box u-flex u-cross-center u-gap-24"
+				in:fly={{ x: -16, y: 0, delay: 350 }}
+				animate:flip={{ duration: 500 }}
+			>
 				<div class="icon-wrapper">
 					<span class={`icon-${status.icon}`} />
 				</div>
@@ -46,7 +77,7 @@
 </div>
 
 <dialog class="modal" bind:this={modalEl}>
-	<form class="modal-form" method="dialog">
+	<form class="modal-form" method="dialog" on:submit={handleSubmit}>
 		<header class="modal-header">
 			<h4 class="modal-title heading-level-5">Add status</h4>
 			<button
