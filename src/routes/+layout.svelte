bbc5d8d31f22e7457c88fe2c184ea333eee898d9
tglide <26071571+TGlide@users.noreply.github.com>
2023-01-17 20:08:06 +0000

    :heavy_plus_sign: add pink deps

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '@appwrite.io/pink/dist/pink.css';
+	import '@appwrite.io/pink-icons/dist/icon.css';
+
+	import '@fontsource/inter/latin-400.css';
+	import '@fontsource/inter/latin-600.css';
+
+	import '@fontsource/poppins/latin-500.css';
+	import '@fontsource/poppins/latin-600.css';
+	import '@fontsource/poppins/latin-700.css';
+
+	import '@fontsource/source-code-pro/latin-400.css';
+</script>
+
+<main>
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class="container"
+	style="border: 1px solid hsl(var(--color-neutral-100)); border-radius: var(--border-radius-small);"
+>
+	<p class="text u-margin-block-start-8">Container</p>
+</div>
